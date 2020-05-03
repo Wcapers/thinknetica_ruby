4aef56dc646f8ecbb16579f066b548967924f780
Wcapers <Mirazizovt@yandex.ru>
2020-05-03 20:01:55 +0300

    accesors done

--- do_op_metaprogramming/modules/acсessors.rb
+++ do_op_metaprogramming/modules/acсessors.rb
@@ -1,4 +1,5 @@
 module Accesors
+
   def attr_accessor_with_history(*names)
       names.each do |name|
         var_name = "@#{name}".to_sym
@@ -12,14 +13,24 @@ module Accesors
         define_method("#{name}_history".to_sym) { return @history[name] }
       end
     end
+
+    def strong_attr_accessor(name, type)
+      var_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) do |value|
+      raise 'Тип не совпадает' unless value.class.to_s == type.to_s
+      instance_variable_set(var_name, value)
+      end
+    end
   end
 
   class Test
     extend Accesors
     attr_accessor_with_history :my_attr, :a, :b, :c
-  end
+    strong_attr_accessor(:q, "Integer")
+    end
   t = Test.new
   t.a= 12
   t.a= 13
   t.a= 14
-  puts "#{t.a_history}"
+  t.q= 123
